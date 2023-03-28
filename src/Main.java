5e13a0ba6003bd8b7446d0a293fbdbc2d3079230
Пользователь <d.elllka@bk.ru>
2023-03-28 15:05:04 +0300

    Объекты

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ public class Main {
         System.out.println("Книга 1");
         Book theShining =  new Book ("С. Кинг",  1997, "Cияние");
         System.out.println("theShining.author = " + theShining.getAuthor());
-        System.out.println("theShining.nameBook = " + theShining.getYear());
+        System.out.println("theShining.year = " + theShining.getYear());
+        System.out.println("theShining.book = " + theShining.getBook());
         Author shining = new Author("Стивен ","Кинг" );
         System.out.println("shining.name = " + shining.getName());
         System.out.println("shining.year = " + shining.getSurname());
@@ -12,11 +13,13 @@ public class Main {
         System.out.println("Книга 2");
         Book theTintenhers = new Book("К. Фукне", 2003, "Чернильное сердце");
         System.out.println("theTintenhers.Author = " + theTintenhers.getAuthor());
-        System.out.println("theTintenhers.nameBook = " + theTintenhers.getYear());
+        System.out.println("theTintenhers.year = " + theTintenhers.getYear());
+        System.out.println("theTintenhers.book = " +theTintenhers.getBook());
         theTintenhers.setYear(2000);
         Author tintenhers = new Author("Корнелия ", "Фукне");
         System.out.println("tintenhers.name = " + tintenhers.getName());
         System.out.println("tintenhers.year = " + tintenhers.getSurname());
         System.out.println("tintenhers.getYear() = " + tintenhers.getSurname());
+
     }
 }
